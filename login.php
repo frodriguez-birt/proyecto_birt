88f85e913782583723a87952ef136918810531ab
frodriguez-birt <91693331+frodriguez-birt@users.noreply.github.com>
2022-01-19 22:01:55 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+include ("conexion.php");
+
+$bd= new BaseDatos();
+$conexion= $bd->conectar();
+
+$mensaje = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $query = 'Select password From empleados where login = "' . $_GET['login'] .'"';
+
+    $res = $bd->seleccionar($query);
+
+    if($row = $res -> fetch_assoc()){
+
+        if (password_verify($_GET['password'],$row['password'])) {
+
+            $mensaje = "login correcto";
+
+        } else {
+
+            $mensaje = "Contraseña incorrecta";
+        }
+
+    } else {
+
+        $mensaje = "El usuario no existe";
+    }
+
+    echo json_encode($mensaje);  
+}
+
+?>
